9616663017a10b3de11fc250da2cd0af657e6b10
toshi6581 <40607607+toshi6581@users.noreply.github.com>
2020-04-02 12:33:22 +0900

    #2 done

--- assessment.js
+++ assessment.js
@@ -6,7 +6,7 @@
   const tweetDivided = document.getElementById('tweet-area');
 
   /**
-  * 指定した要素の子どもを全て除去する
+  * 指定した要素の子どもを全て除去するyo
   * @param {HTMLElement} element HTMLの要素
   */
   function removeAllChildren(element) {
